b60f4343756853e3424da9ba7b65d7febcf155e7
linettej <linettejvalloor@gmail.com>
2023-08-22 17:14:05 +0530

    FEATURE: borrowed books page UI complete

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -2,27 +2,42 @@ import { FC } from 'react';
 import TableHead from '../table-head/TableHead';
 import TableRow from '../table-row/TableRow';
 import './styles.css';
+import TableBookRow from '../table-row-books/tableRowBook';
 
 type TablePropsType = {
   tableHeadValues: any;
   employees?: any;
+  booksReturned?: any;
+  booksBorrowed?: any;
   onRowClick?: (id: number) => void;
   setDeletePopup?: (isVisible: boolean, id: number) => void;
 };
 
-const Table: FC<TablePropsType> = ({ tableHeadValues, employees, onRowClick, setDeletePopup }) => {
+const Table: FC<TablePropsType> = ({
+  booksReturned,
+  booksBorrowed,
+  tableHeadValues,
+  employees,
+  onRowClick,
+  setDeletePopup
+}) => {
   return (
     <table className='table'>
       <TableHead tableHeadValues={tableHeadValues} />
       <tbody className='table-body'>
-        {employees.map((employee) => (
-          <TableRow
-            onRowClick={onRowClick}
-            key={employee.id}
-            employee={employee}
-            setDeletePopup={setDeletePopup}
-          />
-        ))}
+        {employees &&
+          employees.map((employee) => (
+            <TableRow
+              onRowClick={onRowClick}
+              key={employee.id}
+              employee={employee}
+              setDeletePopup={setDeletePopup}
+            />
+          ))}
+        {booksReturned &&
+          booksReturned.map((book) => <TableBookRow key={book.isbn} bookReturn={book} />)}
+        {booksBorrowed &&
+          booksBorrowed.map((book) => <TableBookRow key={book.isbn} bookBorrow={book} />)}
       </tbody>
     </table>
   );
--- src/pages/borrowed-books/borrowedBooks.tsx
+++ src/pages/borrowed-books/borrowedBooks.tsx
@@ -2,20 +2,27 @@
 import SubHeader from '../../components/sub-header/SubHeader';
 import Table from '../../components/table/Table';
 import HomeLayout from '../../layouts/home-layout/HomeLayout';
+import { borrowedBooks, ReturnedBooks } from '../../utils/tmp-borrowed-books';
 
 const BorrowedBooks = () => {
   //to be used to fetch details from emp-book-junction
   //const { empid } = useParams();
+  const booksBorrowed = borrowedBooks;
+  const booksReturned = ReturnedBooks;
 
-  const tableHeadValues = ['Book Title', 'Book ISBN', 'Author', 'Taken On', 'Return Due'];
+  const tableHeadValuesBorrowed = ['Book Title', 'Book ISBN', 'Author', 'Taken On', 'Return'];
+  const tableHeadValuesReturned = ['Book Title', 'Book ISBN', 'Author', 'Taken On', 'Returned On'];
 
   return (
     <HomeLayout>
       <SubHeader title='Borrowed Books' />
-      <Table tableHeadValues={tableHeadValues} />
-      <SubHeader title='Retured Books' />
+      <Table tableHeadValues={tableHeadValuesBorrowed} booksBorrowed={booksBorrowed} />
+      <SubHeader title='Returned Books' />
+      <Table tableHeadValues={tableHeadValuesReturned} booksReturned={booksReturned} />
     </HomeLayout>
   );
 };
+
+// this page should use table , input data to be taken from employee junction table.
 
 export default BorrowedBooks;
