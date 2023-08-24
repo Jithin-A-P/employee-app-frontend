2e15e10801134264e39943e6fed11c2c0d98c5f2
linettej <linettejvalloor@gmail.com>
2023-08-24 20:18:49 +0530

    handling error messages

--- src/pages/create-edit-shelf/createEditShelf.tsx
+++ src/pages/create-edit-shelf/createEditShelf.tsx
@@ -6,15 +6,14 @@ import './style.css';
 import Button from '../../components/button/Button';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useCreateShelfMutation, useEditShelfMutation } from '../../api-client/shelf-api';
-import { getResponseError } from '../../utils/error-formatter';
 
 const CreateShelf = () => {
   const [shelf, setShelf] = useState({
     shelfCode: '',
     location: ''
   });
-
-  const [Error, setError] = useState(null);
+  const [shelfCodeError, setShelfCodeError] = useState(false);
+  const [locationError, setlocationError] = useState(false);
 
   const navigate = useNavigate();
 
@@ -27,24 +26,12 @@ const CreateShelf = () => {
 
   const handleEditShelf = () => {
     console.log(shelf);
-    editShelf({ id: id, body: shelf })
-      .unwrap()
-      .catch((error) => {
-        console.log(error);
+    if (shelf.shelfCode === '') setShelfCodeError(true);
+    else setShelfCodeError(false);
 
-        setError(getResponseError(error));
-        console.log(Error);
-        // issue = JSON.stringify(error.data.errors);
-
-        // // SetErrorMessage(issue);
-
-        // console.log('issue object : ', issue);
-        // alert(
-        //   `Edit Could not be performed! Check input values ! ${JSON.stringify(error.data.errors)}`
-        // );
-        // console.log(`error: ${error.status} ${JSON.stringify(error.data)}`);
-        // console.log('testing  ' + JSON.stringify(error.data.errors['shelfCode']));
-      });
+    if (shelf.location === '') setlocationError(true);
+    else setlocationError(false);
+    editShelf({ id: id, body: shelf });
   };
 
   const [createShelf] = useCreateShelfMutation();
@@ -67,6 +54,9 @@ const CreateShelf = () => {
               label='Shelf Code'
               placeholder='Shelf-Code'
             />
+            {shelfCodeError && (
+              <div className='login-error-message'>Field should not be empty </div>
+            )}
           </div>
           <div className='form-input'>
             <Input
@@ -78,6 +68,7 @@ const CreateShelf = () => {
               label='Shelf Location'
               placeholder='Shelf-location'
             />
+            {locationError && <div className='login-error-message'>Field should not be empty </div>}
           </div>
         </div>
         <div>
