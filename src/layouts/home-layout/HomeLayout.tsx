af0515ba1ae901464fc334409bcab5947b2f383e
linettej <linettejvalloor@gmail.com>
2023-08-25 01:53:11 +0530

    FEATURE : added toasts

--- src/layouts/home-layout/HomeLayout.tsx
+++ src/layouts/home-layout/HomeLayout.tsx
@@ -1,4 +1,6 @@
+import { ToastContainer, toast } from 'react-toastify';
 import Header from '../../components/header/Header';
+import 'react-toastify/dist/ReactToastify.css';
 import Sidebar from '../../components/sidebar/Sidebar';
 import './styles.css';
 
@@ -10,6 +12,11 @@ const HomeLayout = ({ children }) => {
         <Sidebar />
         <div className='home-main-content'>{children}</div>
       </div>
+      <ToastContainer
+        position={toast.POSITION.TOP_CENTER}
+        autoClose={1500}
+        hideProgressBar={true}
+      />
     </div>
   );
 };
