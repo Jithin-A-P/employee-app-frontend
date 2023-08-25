6ede24bf66507517ddb2047429f76c936a849235
Mithun <mithunk094619@gmail.com>
2023-08-25 12:23:43 +0530

    ADDED: card styling

--- src/components/shelf-card/ShelfCard.tsx
+++ src/components/shelf-card/ShelfCard.tsx
@@ -6,11 +6,12 @@ import getCurrentUser from '../../utils/get-current-user';
 export type ShelfCardPropTypes = {
   id?: string;
   location: string;
+  shelfCode: string;
   onClick?: (id: string) => void;
   onViewBooks?: (id: string, e?) => void;
 };
 
-const ShelfCard: FC<ShelfCardPropTypes> = ({ location, onClick, onViewBooks, id }) => {
+const ShelfCard: FC<ShelfCardPropTypes> = ({ location, onClick, onViewBooks, id, shelfCode }) => {
   const currenUserRole = getCurrentUser().role;
   const adminPrivileges = currenUserRole === 'admin' || currenUserRole === 'hr';
 
@@ -20,6 +21,7 @@ const ShelfCard: FC<ShelfCardPropTypes> = ({ location, onClick, onViewBooks, id 
         <img src='../../../assets/img/bookshelf.png' alt='shelf icon' />
       </div>
       <div className='location-button'>
+        <div className='code'>{shelfCode}</div>
         <div className='location'>{location}</div>
         <div className='button-lib'>
           <Button style='library' text='View Books' onClick={(e) => onViewBooks(id, e)} />
